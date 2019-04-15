684412d0ee359643350bd8f578243d008acdf30d
Marc Altmann <marcaltmann@posteo.de>
2019-04-15 20:34:40 +0200

    Replace with project scaffold

--- db/migrate/20190415182659_create_projects.rb
+++ db/migrate/20190415182659_create_projects.rb
@@ -1,7 +1,7 @@
 class CreateProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
-      t.string :name, null: false
+      t.string :name
       t.text :description
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+projects = Project.create([
+  { name: 'Project A' },
+  { name: 'Secret Project' },
+  { name: 'Build prototype' }
+])
