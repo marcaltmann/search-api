937c0adc5737773970fe419e2860474eae057663
Marc Altmann <marcaltmann@posteo.de>
2019-04-16 20:11:30 +0200

    Add AlgoliaSearch gem

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,9 @@
 class Location < ApplicationRecord
+  include AlgoliaSearch
+
   has_and_belongs_to_many :projects
+
+  algoliasearch do
+    # all attributes will be sent
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,14 @@
 class Project < ApplicationRecord
+  include AlgoliaSearch
+
   has_and_belongs_to_many :locations
+
+  algoliasearch do
+    # all attributes will be sent
+    add_attribute :_geoloc
+  end
+
+  def _geoloc
+    locations.map { |l| { lat: l.lat, lng: l.long }}
+  end
 end
